25492b46322c4ae9ed003afa0a29cff67d1ed691
Jtavora <jtavora09@gmail.com>
2024-05-24 09:24:55 -0300

    Atv_fastapi

--- aula_fastapi/app.py
+++ aula_fastapi/app.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from Controller.ClientController import ClientController
 from Controller.DeliveryController import DeliveryController
 from Models.ClientModel import ClientModel
 from Models.DeliveryModel import DeliveryModel
+
+import uvicorn
 
 app = FastAPI()
 
@@ -23,33 +24,39 @@ class Delivery(BaseModel):
     quantity: int
     status: str
 
-@app.get("/")
+@app.get("/", tags=["Root"])
 def return_status():
+    """Retorna uma mensagem de boas-vindas."""
     return {"message": "Welcome to the API!"}
 
-@app.post("/client")
+@app.post("/client", tags=["Clients"])
 def create_client(client: Client):
+    """Cria um novo cliente."""
     client_data = ClientModel(**client.dict())
     client_controller.create(client_data)
-    return {"Client created successfully!"}
+    return {"message": "Client created successfully!"}
 
-@app.post("/delivery")
+@app.post("/delivery", tags=["Delivery"])
 def create_delivery(delivery: Delivery):
+    """Cria uma nova entrega."""
     client = client_controller.get_by_email(delivery.client_email)
     if client is None:
-        return {"message": "Client not found!"}
+        raise HTTPException(status_code=404, detail="Client not found!")
     delivery_data = DeliveryModel(product=delivery.product, quantity=delivery.quantity, status=delivery.status, clients=[client])
     delivery_controller.create(delivery_data)
-    return {"Delivery created successfully!"}
+    return {"message": "Delivery created successfully!"}
 
-@app.get("/clients")
+@app.get("/clients", tags=["Clients"])
 def get_all_clients():
+    """Obtém todos os clientes."""
     clients = client_controller.get_all()
     return {"clients": [client.to_dict() for client in clients]}
 
-@app.get("/deliveries")
+@app.get("/deliveries", tags=["Delivery"])
 def get_all_deliveries():
+    """Obtém todas as entregas."""
     deliveries = delivery_controller.get_all()
     return {"deliveries": [delivery.to_dict() for delivery in deliveries]}
+
 
 uvicorn.run(app, host="localhost", port=8000)
